faaedc15720496361b73f03d502be80fe30fb160
Gabriel Briotto Monteiro <166339851+DevGabrielBrM@users.noreply.github.com>
2024-09-09 22:18:53 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      "/api":{
-        target:"http://localhost:5000"
-      }
-    }
-  }
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: 'frontend/dist', // Diretório onde a build será gerada
+    rollupOptions: {
+      input: {
+        main: 'frontend/index.html', // Certifique-se de que o caminho está correto
+      },
+    },
+  },
+});
